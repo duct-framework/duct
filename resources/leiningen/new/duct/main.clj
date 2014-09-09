afa6347724a19bc2f48c940a569de9136e736377
James Reeves <jreeves@weavejester.com>
2014-09-09 03:59:43 +0100

    Add environ to template

--- resources/leiningen/new/duct/main.clj
+++ resources/leiningen/new/duct/main.clj
@@ -1,7 +1,12 @@
 (ns {{namespace}}.main
   (:gen-class)
   (:require [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]
             [{{namespace}}.system :refer [new-system]]))
 
+(def config
+  {:http {:port (Integer. (env :port "3000"))}})
+
 (defn -main [& args]
-  (component/start (new-system {})))
+  (println "Starting HTTP server on port" (-> config :http :port))
+  (component/start (new-system config)))
--- resources/leiningen/new/duct/project.clj
+++ resources/leiningen/new/duct/project.clj
@@ -4,8 +4,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
                  [compojure "1.1.9"]
+                 [environ "1.0.0"]
                  [ring "1.3.1"]
                  [ring/ring-defaults "0.1.1"]
                  [ring-jetty-component "0.2.0"]]
+  :plugins [[lein-environ "1.0.0"]]
   :main ^:skip-aot {{namespace}}.main
-  :profiles {:uberjar {:aot :all}})
+  :profiles
+  {:uberjar {:aot :all}
+   :dev     {:env {:port 3000}}})
