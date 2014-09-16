e9f87f831e09a20195af5e07abf893ab1d8dcaa5
James Reeves <jreeves@weavejester.com>
2014-09-16 19:26:04 -0400

    Allow custom user middleware

--- lein-template/resources/leiningen/new/duct/system.clj
+++ lein-template/resources/leiningen/new/duct/system.clj
@@ -4,12 +4,12 @@
             [duct.support :as support]
             [{{namespace}}.handler :refer [new-handler]]))
 
-(def default-config
+(def base-config
   {:app  {:new-handler new-handler}
    :http {:port 3000}})
 
 (defn new-system [config]
-  (let [config (merge default-config config)]
+  (let [config (merge base-config config)]
     (component/system-map
      :app  (support/app-component (:app config))
      :http (-> (jetty-server (:http config))
--- lein-template/resources/leiningen/new/duct/user.clj
+++ lein-template/resources/leiningen/new/duct/user.clj
@@ -5,12 +5,18 @@
             [clojure.java.io :as io]
             [com.stuartsierra.component :as component]
             [reloaded.repl :refer [system init start stop go reset]]
-            [{{namespace}}.system :refer [new-system]]))
+            [ring.middleware.stacktrace :as ring-stacktrace]
+            [{{namespace}}.system :as system]))
+
+(defn new-handler [config]
+  (-> ((-> system/base-config :app :new-handler) config)
+      (ring-stacktrace/wrap-stacktrace)))
 
 (def config
-  {:http {:port 3000}})
+  {:app  {:new-handler new-handler}
+   :http {:port 3000}})
 
 (when (io/resource "local.clj")
   (load "local"))
 
-(reloaded.repl/set-init! #(new-system config))
+(reloaded.repl/set-init! #(system/new-system config))
