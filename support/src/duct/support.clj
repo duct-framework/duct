3a3ac50af27b38447973ebb045cee0fb65f1f0f8
James Reeves <jreeves@weavejester.com>
2014-09-19 03:03:48 -0400

    Rearrange duct.support

--- support/src/duct/support.clj
+++ support/src/duct/support.clj
@@ -2,19 +2,6 @@
   (:require [com.stuartsierra.component :as component]
             [ring.util.response :as response]))
 
-(defrecord Application [new-handler middleware]
-  component/Lifecycle
-  (start [component]
-    (if-not (:handler component)
-      (let [wrap-middleware (apply comp identity (reverse middleware))]
-        (assoc component :handler (wrap-middleware (new-handler component))))
-      component))
-  (stop [component]
-    (dissoc component :handler)))
-
-(defn app-component [options]
-  (map->Application options))
-
 (defn html-resource [resource]
   (-> (response/resource-response resource)
       (response/content-type "text/html")))
@@ -30,4 +17,15 @@
            (-> (html-resource error-resource)
                (response/status 500)))))))
 
+(defrecord Application [new-handler middleware]
+  component/Lifecycle
+  (start [component]
+    (if-not (:handler component)
+      (let [wrap-middleware (apply comp identity (reverse middleware))]
+        (assoc component :handler (wrap-middleware (new-handler component))))
+      component))
+  (stop [component]
+    (dissoc component :handler)))
 
+(defn app-component [options]
+  (map->Application options))
