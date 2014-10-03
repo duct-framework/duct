ed14d3f8968a24a2a281f4c5ef4e116c74033ebe
James Reeves <jreeves@weavejester.com>
2014-10-03 19:44:32 +0100

    Rename duct/support project to just duct

--- duct/project.clj
+++ duct/project.clj
@@ -1,4 +1,4 @@
-(defproject duct/support "0.1.0-SNAPSHOT"
+(defproject duct "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
