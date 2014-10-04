dd59f0904f37ae827d4c8bc8908e19dd8c3c9185
James Reeves <jreeves@weavejester.com>
2014-10-04 01:51:56 +0100

    Remove opinionated logging for now

--- duct/project.clj
+++ duct/project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.stuartsierra/component "0.2.2"]
-                 [ring/ring-core "1.3.1"]
-                 [com.taoensso/timbre "3.3.1"]])
+                 [ring/ring-core "1.3.1"]])
--- lein-template/resources/leiningen/new/duct/main.clj
+++ lein-template/resources/leiningen/new/duct/main.clj
@@ -3,14 +3,11 @@
   (:require [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
             [duct.middleware.errors :refer [wrap-hide-errors]]
-            [duct.middleware.logging :refer [wrap-log-requests wrap-log-errors]]
             [{{namespace}}.system :refer [new-system]]))
 
 (def config
   {:http {:port (Integer. (env :port "3000"))}
-   :app  {:middleware [wrap-log-requests
-                       wrap-log-errors
-                       wrap-hide-errors]}})
+   :app  {:middleware [wrap-hide-errors]}})
 
 (defn -main [& args]
   (println "Starting HTTP server on port" (-> config :http :port))
