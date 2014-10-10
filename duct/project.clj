21cf2417342cc4c5b9498093463ac9cc1380a831
James Reeves <jreeves@weavejester.com>
2014-10-10 16:04:22 +0100

    Add Clojure version to Duct library

--- duct/project.clj
+++ duct/project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.stuartsierra/component "0.2.2"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.stuartsierra/component "0.2.2"]
                  [ring/ring-core "1.3.1"]])
