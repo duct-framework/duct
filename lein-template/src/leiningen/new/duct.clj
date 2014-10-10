80cb1a0cded636be8a1709cbf79eb49139933f9a
James Reeves <jreeves@weavejester.com>
2014-10-10 18:23:51 +0100

    Improve leiningen.new.duct formatting

--- lein-template/src/leiningen/new/duct.clj
+++ lein-template/src/leiningen/new/duct.clj
@@ -20,14 +20,20 @@
              ["project.clj"  (render "project.clj" data)]
              [".gitignore"   (render "gitignore" data)]
              ["dev/user.clj" (render "user.clj" data)]
+
              ["profiles.clj.sample"  (render "profiles.clj" data)]
              ["dev/local.clj.sample" (render "local.clj" data)]
+
              ["src/{{nested-dirs}}/main.clj"   (render "main.clj" data)]
              ["src/{{nested-dirs}}/system.clj" (render "system.clj" data)]
+
              "src/{{nested-dirs}}/component"
+             "src/{{nested-dirs}}/endpoint"
+
              ["src/{{nested-dirs}}/endpoint/example.clj" (render "example.clj" data)]
              ["test/{{nested-dirs}}/endpoint/example_test.clj"
               (render "example_test.clj" data)]
+
              ["resources/public/favicon.ico"
               (io/input-stream (io/resource "leiningen/new/duct/favicon.ico"))]
              ["resources/public/404.html"     (render "404.html" data)]
