77b689d301f5e9daf8108708dfa93bfd0561a330
James Reeves <jreeves@weavejester.com>
2014-10-11 21:13:56 +0100

    Add description, URL and SCM info to subprojects

--- duct/project.clj
+++ duct/project.clj
@@ -1,6 +1,7 @@
 (defproject duct "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Support library for the Duct template."
+  :url "https://github.com/weavejester/duct"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,6 +1,7 @@
 (defproject duct/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A template for a component-based web application."
+  :url "https://github.com/weavejester/duct"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
