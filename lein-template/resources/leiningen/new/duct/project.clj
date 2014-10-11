9ec78dadec66a926b11ddcc1185f5a74d01e8455
James Reeves <jreeves@weavejester.com>
2014-10-11 04:51:36 +0100

    Add min-lein-version to project template

--- lein-template/resources/leiningen/new/duct/project.clj
+++ lein-template/resources/leiningen/new/duct/project.clj
@@ -1,6 +1,7 @@
 (defproject {{raw-name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
                  [compojure "1.2.0"]
