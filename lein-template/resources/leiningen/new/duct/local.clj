06809baf93570ab7331f18f76bbb73e48e5af71d
James Reeves <jreeves@weavejester.com>
2014-10-13 01:04:13 +0100

    Remove trailing space in template file

--- lein-template/resources/leiningen/new/duct/local.clj
+++ lein-template/resources/leiningen/new/duct/local.clj
@@ -1,6 +1,6 @@
 ;; This is a sample file. Copy this file to dev/local.clj.
 ;; This file will be loaded by the user namespace, and should contain
-;; REPL configuration you want kept out of source control. 
+;; REPL configuration you want kept out of source control.
 
 (def config
   {:http {:port 3000}})
