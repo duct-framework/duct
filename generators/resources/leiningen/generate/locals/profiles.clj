26555f99871777169d333303b4cf778b603eef4f
James Reeves <jreeves@weavejester.com>
2014-12-20 18:38:53 +0000

    Tidy up files with custom template syntax

--- generators/resources/leiningen/generate/locals/profiles.clj
+++ generators/resources/leiningen/generate/locals/profiles.clj
@@ -1,6 +1,5 @@
+{{=<< >>=}}
 ;; Local profile overrides
 
-{{=<< >>=}}
-{:profiles/dev  {:env {<<#database-url>>:database-url "<<database-url>>"<</database-url>>}}
+{:profiles/dev  {:env <<#database-url>>{:database-url "<<database-url>>"}<</database-url>>}
  :profiles/test {}}
-<<={{ }}=>>
--- lein-template/resources/leiningen/new/duct/example/endpoint.clj
+++ lein-template/resources/leiningen/new/duct/example/endpoint.clj
@@ -1,14 +1,13 @@
-(ns {{namespace}}.endpoint.example
+{{=<< >>=}}
+(ns <<namespace>>.endpoint.example
   (:require [compojure.core :refer :all]
             [clojure.java.io :as io]))
 
-{{#site?}}
+<<#site?>>
 (def welcome-page
-  (io/resource "{{dirs}}/endpoint/example/welcome.html"))
+  (io/resource "<<dirs>>/endpoint/example/welcome.html"))
 
-{{/site?}}
-{{=<< >>=}}
+<</site?>>
 (defn example-endpoint [<<#jdbc?>>{{db :spec} :db}<</jdbc?>><<^jdbc?>>config<</jdbc?>>]
   (routes
    (GET "/" [] <<^site?>>"Hello World"<</site?>><<#site?>>welcome-page<</site?>>)))
-<<={{ }}=>>
