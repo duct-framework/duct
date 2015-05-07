fdd19d32fd6cdd59637c280114855b0aebecd4f9
James Reeves <jreeves@weavejester.com>
2015-05-07 17:48:54 +0100

    Update dependencies

--- duct/project.clj
+++ duct/project.clj
@@ -6,6 +6,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [com.stuartsierra/component "0.2.2"]
-                 [compojure "1.3.1"]
+                 [com.stuartsierra/component "0.2.3"]
+                 [compojure "1.3.3"]
                  [ring/ring-core "1.3.2"]])
--- lein-template/resources/leiningen/new/duct/base/project.clj
+++ lein-template/resources/leiningen/new/duct/base/project.clj
@@ -3,19 +3,19 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.stuartsierra/component "0.2.2"]
-                 [compojure "1.3.1"]
+                 [com.stuartsierra/component "0.2.3"]
+                 [compojure "1.3.3"]
                  [duct "0.1.0"]
                  [environ "1.0.0"]{{#heroku?}}{{#jdbc?}}
                  [hanami "0.1.0"]{{/jdbc?}}{{/heroku?}}
                  [meta-merge "0.1.1"]
                  [ring "1.3.2"]
-                 [ring/ring-defaults "0.1.3"]
+                 [ring/ring-defaults "0.1.5"]
                  [ring-jetty-component "0.2.2"]{{#site?}}
                  [ring-webjars "0.1.0"]
-                 [org.webjars/normalize.css "3.0.1"]{{/site?}}{{#jdbc?}}
+                 [org.webjars/normalize.css "3.0.2"]{{/site?}}{{#jdbc?}}
                  [duct/hikaricp-component "0.1.0"]{{/jdbc?}}{{#postgres?}}
-                 [org.postgresql/postgresql "9.3-1102-jdbc4"]{{/postgres?}}]
+                 [org.postgresql/postgresql "9.4-1201-jdbc4"]{{/postgres?}}]
   :plugins [[lein-environ "1.0.0"]
             [lein-gen "0.2.2"]]
   :generators [[duct/generators "0.1.0"]]
@@ -36,7 +36,7 @@
    :project/dev   {:source-paths ["dev"]
                    :repl-options {:init-ns user}
                    :dependencies [[reloaded.repl "0.1.0"]
-                                  [org.clojure/tools.namespace "0.2.8"]
-                                  [kerodon "0.5.0"]]
+                                  [org.clojure/tools.namespace "0.2.10"]
+                                  [kerodon "0.6.1"]]
                    :env {:port 3000}}
    :project/test  {}})
