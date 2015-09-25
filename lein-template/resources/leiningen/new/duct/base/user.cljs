c276995d257e5d9b42cfec22b122d6d8e47234e5
James Reeves <jreeves@weavejester.com>
2015-09-25 21:08:55 +0100

    Add Figwheel client to cljs.user namespace

--- lein-template/resources/leiningen/new/duct/base/user.cljs
+++ lein-template/resources/leiningen/new/duct/base/user.cljs
@@ -1,5 +1,8 @@
-(ns cljs.user)
+(ns cljs.user
+  (:require [figwheel.client :as figwheel]))
 
 (js/console.info "Starting in development mode")
 
 (enable-console-print!)
+
+(figwheel/start {:websocket-url "ws://localhost:3449/figwheel-ws"})
