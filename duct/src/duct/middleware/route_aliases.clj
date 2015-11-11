10a9278296414398ba8ce5a90d9f5b98b0fc3f18
James Reeves <jreeves@weavejester.com>
2015-11-11 02:48:39 +0000

    Change wrap-route-aliases to use :uri key

--- duct/src/duct/middleware/route_aliases.clj
+++ duct/src/duct/middleware/route_aliases.clj
@@ -1,9 +1,7 @@
-(ns duct.middleware.route-aliases
-  (:require [ring.util.request :as request]))
+(ns duct.middleware.route-aliases)
 
 (defn wrap-route-aliases [handler aliases]
   (fn [request]
-    (let [path (request/path-info request)]
-      (if-let [alias (aliases path)]
-        (handler (assoc request :path-info alias))
-        (handler request)))))
+    (if-let [alias (aliases (:uri request))]
+      (handler (assoc request :uri alias))
+      (handler request))))
