6b3be4418c8961289692331858a24cb9e459596b
James Reeves <jreeves@weavejester.com>
2015-11-13 05:44:49 +0000

    Refactor example endpoint with context

--- lein-template/resources/leiningen/new/duct/example/endpoint.clj
+++ lein-template/resources/leiningen/new/duct/example/endpoint.clj
@@ -1,13 +1,9 @@
 {{=<< >>=}}
 (ns <<namespace>>.endpoint.example
-  (:require [compojure.core :refer :all]
-            [clojure.java.io :as io]))
+  (:require [compojure.core :refer :all]<<#site?>>
+            [clojure.java.io :as io]<</site?>>))
 
-<<#site?>>
-(def example-page
-  (io/resource "<<dirs>>/endpoint/example/example.html"))
-
-<</site?>>
 (defn example-endpoint [<<#jdbc?>>{{db :spec} :db}<</jdbc?>><<^jdbc?>>config<</jdbc?>>]
-  (routes
-   (GET "/example" [] <<^site?>>"This is an example endpoint"<</site?>><<#site?>>example-page<</site?>>)))
+  (context "/example" []
+    (GET "/" []
+      <<^site?>>"This is an example endpoint"<</site?>><<#site?>>(io/resource "<<dirs>>/endpoint/example/example.html")<</site?>>)))
