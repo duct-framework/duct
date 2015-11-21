ede87ef9e47f8c486d24914ca1e9030423db90da
James Reeves <jreeves@weavejester.com>
2015-11-21 19:22:39 +0000

    Release 0.5.2

--- generators/project.clj
+++ generators/project.clj
@@ -1,4 +1,4 @@
-(defproject duct/generators "0.5.1"
+(defproject duct/generators "0.5.2"
   :description "Duct generators for lein-generate"
   :url "https://github.com/weavejester/duct"
   :scm {:dir ".."}
