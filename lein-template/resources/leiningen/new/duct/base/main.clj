ba6df2d7e4bcc4ffa255d4c97df1f65671767ea9
eunmin <telepopsound@gmail.com>
2015-11-29 14:39:47 +0900

    Add shutdown hook for stopping system

--- lein-template/resources/leiningen/new/duct/base/main.clj
+++ lein-template/resources/leiningen/new/duct/base/main.clj
@@ -3,6 +3,7 @@
   (:require {{#site?}}[clojure.java.io :as io]
             {{/site?}}[com.stuartsierra.component :as component]
             [duct.middleware.errors :refer [wrap-hide-errors]]
+            [duct.util.runtime :refer [add-shutdown-hook]]
             [meta-merge.core :refer [meta-merge]]
             [{{namespace}}.config :as config]
             [{{namespace}}.system :refer [new-system]]))
@@ -19,4 +20,7 @@
 (defn -main [& args]
   (let [system (new-system config)]
     (println "Starting HTTP server on port" (-> system :http :port))
+    (add-shutdown-hook ::stop-system #(do
+                                        (println "Stopping HTTP server")
+                                        (component/stop system)))
     (component/start system)))
