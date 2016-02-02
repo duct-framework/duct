f4998f0bb0ef5bbbe9fee9a04c000efc14bb2b10
James Reeves <jreeves@weavejester.com>
2016-02-02 03:14:38 +0000

    Update template dependencies

--- lein-template/resources/leiningen/new/duct/base/project.clj
+++ lein-template/resources/leiningen/new/duct/base/project.clj
@@ -2,25 +2,25 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]{{#cljs?}}
+  :dependencies [[org.clojure/clojure "1.8.0"]{{#cljs?}}
                  [org.clojure/clojurescript "1.7.228"]{{/cljs?}}
                  [com.stuartsierra/component "0.3.0"]
                  [compojure "1.4.0"]
                  [duct "0.5.7"]
-                 [environ "1.0.1"]{{#heroku?}}{{#jdbc?}}
+                 [environ "1.0.2"]{{#heroku?}}{{#jdbc?}}
                  [hanami "0.1.0"]{{/jdbc?}}{{/heroku?}}
                  [meta-merge "0.1.1"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring-jetty-component "0.3.0"]{{#site?}}
                  [ring-webjars "0.1.1"]
-                 [org.slf4j/slf4j-nop "1.7.12"]
+                 [org.slf4j/slf4j-nop "1.7.14"]
                  [org.webjars/normalize.css "3.0.2"]{{/site?}}{{#jdbc?}}
                  [duct/hikaricp-component "0.1.0"]{{/jdbc?}}{{#postgres?}}
-                 [org.postgresql/postgresql "9.4-1203-jdbc4"]{{/postgres?}}{{#sqlite?}}
+                 [org.postgresql/postgresql "9.4.1207"]{{/postgres?}}{{#sqlite?}}
                  [org.xerial/sqlite-jdbc "3.8.11.2"]{{/sqlite?}}{{#ragtime?}}
                  [duct/ragtime-component "0.1.3"]{{/ragtime?}}]
-  :plugins [[lein-environ "1.0.1"]
+  :plugins [[lein-environ "1.0.2"]
             [lein-gen "0.2.2"]{{#cljs?}}
             [lein-cljsbuild "1.1.2"]{{/cljs?}}]
   :generators [[duct/generators "0.5.7"]]
