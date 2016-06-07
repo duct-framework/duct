5e1ad63c5c99b86477a3c2946795a131bd6216f9
James Reeves <jreeves@weavejester.com>
2016-06-07 20:37:52 +0100

    Add imports to config reader

--- duct/project.clj
+++ duct/project.clj
@@ -8,6 +8,7 @@
                  [org.clojure/tools.macro "0.1.5"]
                  [com.stuartsierra/component "0.3.1"]
                  [compojure "1.5.0"]
+                 [environ "1.0.3"]
                  [ring/ring-core "1.4.0"]]
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
--- duct/src/duct/util/config.clj
+++ duct/src/duct/util/config.clj
@@ -1,6 +1,7 @@
 (ns duct.util.config
   (:require [clojure.edn :as edn]
-            [clojure.walk :as walk]))
+            [clojure.walk :as walk]
+            [environ.core :refer [env]]))
 
 (defrecord Ref [keys])
 
@@ -10,6 +11,9 @@
 (defmethod reader 'ref [_ _ value]
   (->Ref value))
 
+(defmethod reader 'import [{:keys [imports]} _ value]
+  (get-in imports value))
+
 (defn- resolve-refs-once [config]
   (walk/postwalk #(if (instance? Ref %) (get-in config (:keys %)) %) config))
 
@@ -17,10 +21,14 @@
   (let [config' (resolve-refs-once config)]
     (if (= config config') config (recur config'))))
 
+(def default-options
+  {:imports {:env env}})
+
 (defn read-config
   ([source]
    (read-config source {}))
   ([source options]
-   (->> (slurp source)
-        (edn/read-string {:default (partial reader options)})
-        (resolve-refs))))
+   (let [options (merge default-options options)]
+     (->> (slurp source)
+          (edn/read-string {:default (partial reader options)})
+          (resolve-refs)))))
