b20573ac841b7c190a09567da2877de21f579949
James Reeves <jreeves@weavejester.com>
2016-06-07 20:17:21 +0100

    Add refs to config reader

--- duct/src/duct/util/config.clj
+++ duct/src/duct/util/config.clj
@@ -1,5 +1,26 @@
 (ns duct.util.config
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.walk :as walk]))
 
-(defn read-config [source]
-  (edn/read-string (slurp source)))
+(defrecord Ref [keys])
+
+(defmulti reader
+  (fn [options tag value] tag))
+
+(defmethod reader 'ref [_ _ value]
+  (->Ref value))
+
+(defn- resolve-refs-once [config]
+  (walk/postwalk #(if (instance? Ref %) (get-in config (:keys %)) %) config))
+
+(defn- resolve-refs [config]
+  (let [config' (resolve-refs-once config)]
+    (if (= config config') config (recur config'))))
+
+(defn read-config
+  ([source]
+   (read-config source {}))
+  ([source options]
+   (->> (slurp source)
+        (edn/read-string {:default (partial reader options)})
+        (resolve-refs))))
