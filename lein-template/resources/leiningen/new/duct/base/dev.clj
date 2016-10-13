75202011075365363e773eff7f9bd6377afe7239
James Reeves <jreeves@weavejester.com>
2016-10-13 19:07:57 +0100

    Add logging setup to dev namespace

--- lein-template/resources/leiningen/new/duct/base/dev.clj
+++ lein-template/resources/leiningen/new/duct/base/dev.clj
@@ -7,19 +7,27 @@
             [com.stuartsierra.component :as component]
             [duct.generate :as gen]
             [duct.util.config :as config]
+            [duct.util.logging :as logging]
             [duct.util.repl :refer [setup test cljs-repl migrate rollback]]
             [duct.util.system :as system]
-            [reloaded.repl :refer [system init start stop go reset]]))
+            [reloaded.repl :refer [system init start stop go reset]]
+            [taoensso.timbre :as log]))
 
-(defn build-system []
+(defn read-config []
   (->> ["{{dirs}}/config.edn" "dev.edn" "local.edn"]
        (keep io/resource)
-       (apply config/read)
-       (system/build)))
+       (apply config/read)))
+
+(defn load-system []
+  (let [config (read-config)]
+    (logging/set-config! config)
+    (system/build config)))
 
 (when (io/resource "local.clj")
   (load "local"))
 
 (gen/set-ns-prefix '{{namespace}})
 
-(reloaded.repl/set-init! build-system)
+(logging/set-config! (read-config))
+
+(reloaded.repl/set-init! load-system)
