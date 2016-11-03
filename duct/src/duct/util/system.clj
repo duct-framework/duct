f1d72e20a3327a5991e45dd58f13f55b377c1f93
James Reeves <jreeves@weavejester.com>
2016-11-03 22:36:19 +0000

    Use keyword lookup to simply component config

--- duct/src/duct/util/system.clj
+++ duct/src/duct/util/system.clj
@@ -1,27 +1,17 @@
 (ns duct.util.system
   (:require [com.stuartsierra.component :as component]
-            [duct.component.endpoint :refer [endpoint-component]]
-            [duct.util.namespace :as ns]))
+            [duct.component.endpoint :refer [endpoint-component]]))
 
-(defn- add-components [system components config]
-  (reduce-kv (fn [m k v] (assoc m k ((ns/resolve-var v) (config k)))) system components))
+(defn- add-components [system components]
+  (reduce-kv (fn [m k v] (assoc m k ((:constructor v) (:options v)))) system components))
 
 (defn- add-endpoints [system endpoints]
-  (reduce-kv (fn [m k v] (assoc m k (endpoint-component (ns/resolve-var v))))
-             system
-             endpoints))
-
-(defn- dissoc-all [m ks]
-  (apply dissoc m ks))
+  (reduce-kv (fn [m k v] (assoc m k (endpoint-component v))) system endpoints))
 
 (defn build [{:keys [system/components
                      system/endpoints
-                     system/dependencies
-                     system/config]}]
+                     system/dependencies]}]
   (-> (component/system-map)
-      (add-components components config)
+      (add-components components)
       (add-endpoints endpoints)
-      (component/system-using dependencies)
-      (into (-> config
-                (dissoc-all (keys components))
-                (dissoc-all (keys endpoints))))))
+      (component/system-using dependencies)))
