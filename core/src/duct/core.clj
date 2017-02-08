8486b0ce4819ad6d667d32f2788b54448b41481e
James Reeves <jreeves@weavejester.com>
2017-02-08 22:39:18 +0000

    Add docstrings to core

--- core/src/duct/core.clj
+++ core/src/duct/core.clj
@@ -1,4 +1,5 @@
 (ns duct.core
+  "Core functions required by a Duct application."
   (:require [clojure.java.io :as io]
             [integrant.core :as ig]
             [meta-merge.core :refer [meta-merge]]))
@@ -11,23 +12,36 @@
 (defonce ^:private init-shutdown-hook
   (delay (.addShutdownHook (Runtime/getRuntime) (Thread. #'run-hooks))))
 
-(defn add-shutdown-hook [k f]
+(defn add-shutdown-hook
+  "Set a function to be executed when the current process shuts down. The key
+  argument should be unique, and is used in remove-shutdown-hook."
+  [key func]
   (force init-shutdown-hook)
-  (swap! hooks assoc k f))
+  (swap! hooks assoc key func))
 
-(defn remove-shutdown-hook [k]
+(defn remove-shutdown-hook
+  "Remove a shutdown hook identified by the specified key."
+  [key]
   (swap! hooks dissoc k))
 
-(defn not-in? [m ks]
+(defn not-in?
+  "Return true if the map, m, does not contain a nested value identified by a
+  sequence of keys, ks."
+  [m ks]
   (let [o (Object.)] (identical? (get-in m ks o) o)))
 
-(defn assoc-in-default [m ks default]
+(defn assoc-in-default
+  "Functionally the same as assoc-in, except that it will not overwrite any
+  existing value."
+  [m ks default]
   (cond-> m (not-in? m ks) (assoc-in ks default)))
 
 (def ^:private readers
   {'resource io/resource})
 
 (defn read-config
+  "Read a configuration from one or more slurpable sources. Multiple sources
+  are meta-merged together."
   ([source]
    (ig/read-string {:readers readers} (slurp source)))
   ([source & sources]
@@ -39,13 +53,19 @@
       (modules config))
     config))
 
-(defn prep [config]
+(defn prep
+  "Prep a configuration, ready to be initiated. Key namespaces are loaded,
+  and modules are applied."
+  [config]
   (-> config
       (doto ig/load-namespaces)
       (apply-modules)
       (doto ig/load-namespaces)))
 
-(defn exec [config]
+(defn exec
+  "Prep then initiate a configuration, and block indefinitely. This function
+  should be called from -main when a standalone application is required."
+  [config]
   (let [system (-> config prep ig/init)]
     (add-shutdown-hook ::exec #(ig/halt! system))
     (.. Thread currentThread join)))
