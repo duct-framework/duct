4fa6a0b588cdc8b2456c1fc386c382d9a54988a6
James Reeves <jreeves@weavejester.com>
2017-02-11 04:39:15 +0000

    Clean unused parts of project.clj template

--- lein-template/resources/leiningen/new/duct/base/project.clj
+++ lein-template/resources/leiningen/new/duct/base/project.clj
@@ -2,54 +2,21 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]{{#cljs?}}
-                 [org.clojure/clojurescript "1.8.51"]{{/cljs?}}
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/core "0.9.0-SNAPSHOT"]
                  [duct/module.logging "0.1.0-SNAPSHOT"]
-                 [duct/module.web "0.1.0-SNAPSHOT"]
-                 [integrant "0.2.1"]{{#jdbc?}}
-                 [duct/hikaricp-component "0.1.0"]{{/jdbc?}}{{#postgres?}}
-                 [org.postgresql/postgresql "9.4.1211"]{{/postgres?}}{{#sqlite?}}
-                 [org.xerial/sqlite-jdbc "3.14.2.1"]{{/sqlite?}}{{#ragtime?}}
-                 [duct/ragtime-component "0.1.4"]{{/ragtime?}}]
-  :plugins [{{#cljs?}}[lein-cljsbuild "1.1.2"]{{/cljs?}}]
+                 [duct/module.web "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot {{namespace}}.main{{#uberjar-name}}
   :uberjar-name "{{uberjar-name}}"{{/uberjar-name}}
-  :target-path "target/%s/"{{#cljs?}}
-  :resource-paths ["resources" "target/cljsbuild"]
-  :prep-tasks [["javac"] ["cljsbuild" "once"] ["compile"]]
-  :cljsbuild
-  {:builds
-   [{:id "main"
-     :jar true
-     :source-paths ["src"]
-     :compiler
-     {:output-to     "target/cljsbuild/{{dirs}}/public/js/main.js"
-      :optimizations :advanced}}]}{{/cljs?}}
-  :aliases {"setup"  ["run" "-m" "duct.util.repl/setup"]{{#heroku?}}
-            "deploy" ["do"
-                      ["vcs" "assert-committed"]
-                      ["vcs" "push" "heroku" "master"]]{{/heroku?}}}
+  :target-path "target/%s/"
   :profiles
   {:dev  [:project/dev  :profiles/dev]
-   :test [:project/test :profiles/test]{{#cljs?}}
-   :repl {:resource-paths ^:replace ["resources" "dev/resources" "target/figwheel"]
-          :prep-tasks     ^:replace [["javac"] ["compile"]]}{{/cljs?}}
+   :test [:project/test :profiles/test]
    :uberjar {:aot :all}
    :profiles/dev  {}
    :profiles/test {}
-   :project/dev   {:dependencies [[integrant/repl "0.1.0"]
-                                  [org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/tools.nrepl "0.2.12"]
-                                  [eftest "0.1.1"]
-                                  [com.gearswithingears/shrubbery "0.4.1"]
-                                  [kerodon "0.8.0"]{{#cljs?}}
-                                  [binaryage/devtools "0.8.2"]
-                                  [com.cemerick/piggieback "0.2.1"]
-                                  [duct/figwheel-component "0.3.3"]
-                                  [figwheel "0.5.8"]{{/cljs?}}]
+   :project/dev   {:dependencies   [[integrant/repl "0.1.0"]]
                    :source-paths   ["dev/src"]
                    :resource-paths ["dev/resources"]
-                   :repl-options {:init-ns user{{#cljs?}}
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]{{/cljs?}}}}
+                   :repl-options   {:init-ns user}}
    :project/test  {}})
