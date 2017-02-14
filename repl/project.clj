de4187a7168cb11a9da2d65741ad0dd0ce360115
James Reeves <jreeves@weavejester.com>
2017-02-14 18:17:05 +0000

    Add duct/repl project

---
+++ repl/project.clj
@@ -0,0 +1,6 @@
+(defproject duct/repl "0.9.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
