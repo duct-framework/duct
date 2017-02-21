f24666c0f9509b80fcf6182f5e189dba718e04bb
James Reeves <jreeves@weavejester.com>
2017-02-21 18:28:22 +0000

    Add lein-plugin project

---
+++ lein-plugin/project.clj
@@ -0,0 +1,6 @@
+(defproject duct/lein-plugin "0.9.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
---
+++ lein-plugin/src/leiningen/duct.clj
@@ -0,0 +1,6 @@
+(ns leiningen.duct)
+
+(defn duct
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
