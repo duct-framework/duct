7e0106bdf843f16c4ee5d6d3710cfaad4f7f2a1a
James Reeves <jreeves@weavejester.com>
2017-02-21 18:42:35 +0000

    Add setup subtask to lein-plugin

--- lein-plugin/src/leiningen/duct.clj
+++ lein-plugin/src/leiningen/duct.clj
@@ -1,6 +1,17 @@
-(ns leiningen.duct)
+(ns leiningen.duct
+  (:require [clojure.java.io :as io]
+            [leiningen.core.main :as main]))
 
-(defn duct
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+(defn- copy-resource [resource path]
+  (io/copy (io/input-stream (io/resource resource)) (io/file path))
+  (println "Created" path))
+
+(defn setup [_]
+  (copy-resource "leiningen/duct/profiles.clj" "profiles.clj")
+  (copy-resource "leiningen/duct/local.edn" "dev/resources/local.edn")
+  (copy-resource "leiningen/duct/local.clj" "dev/src/local.clj"))
+
+(defn duct [project subtask & args]
+  (case subtask
+    "setup" (apply setup project args)
+    (main/abort "Unknown duct subtask:" subtask)))
