15d65c235b0c05816b3d8df72a80335c4080bd82
James Reeves <jreeves@weavejester.com>
2017-02-21 21:14:47 +0000

    Add compile subtask to lein-plugin

--- lein-plugin/src/leiningen/duct.clj
+++ lein-plugin/src/leiningen/duct.clj
@@ -1,6 +1,8 @@
 (ns leiningen.duct
+  (:refer-clojure :exclude [compile])
   (:require [clojure.java.io :as io]
-            [leiningen.core.main :as main]))
+            [leiningen.core.main :as main]
+            [leiningen.core.eval :as eval]))
 
 (defn- copy-resource [resource path]
   (io/copy (io/input-stream (io/resource resource)) (io/file path))
@@ -11,7 +13,14 @@
   (copy-resource "leiningen/duct/local.edn" "dev/resources/local.edn")
   (copy-resource "leiningen/duct/local.clj" "dev/src/local.clj"))
 
+(defn compile [{{:keys [configs]} :duct :as project}]
+  (eval/eval-in-project
+   project
+   `(duct.core/compile (doto (duct.core/read-config ~@configs) prn))
+   `(require 'duct.core)))
+
 (defn duct [project subtask & args]
   (case subtask
-    "setup" (apply setup project args)
+    "setup"   (apply setup project args)
+    "compile" (apply compile project args)
     (main/abort "Unknown duct subtask:" subtask)))
