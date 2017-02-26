0327671b6b65cd4ba0f08d31cc3e8c38eb1394e9
James Reeves <jreeves@weavejester.com>
2017-02-26 22:19:07 +0000

    Rename duct/lein-plugin to duct/lein-duct
    Middleware and hooks have to be placed in a namespace called
    lein-plugin.plugin for a project named duct/lein-plugin. This namespace
    is too generic. By changing the project name to duct/lein-duct, a
    namespace of lein-duct.plugin can be used instead.

--- lein-duct/project.clj
+++ lein-duct/project.clj
@@ -1,4 +1,4 @@
-(defproject duct/lein-plugin "0.9.0-SNAPSHOT"
+(defproject duct/lein-duct "0.9.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- lein-template/resources/leiningen/new/duct/base/project.clj
+++ lein-template/resources/leiningen/new/duct/base/project.clj
@@ -7,7 +7,7 @@
                  [duct/module.logging "0.1.0-SNAPSHOT"]
                  [duct/module.web "0.1.0-SNAPSHOT"]{{#cljs?}}
                  [duct/module.cljs "0.1.0-SNAPSHOT"]{{/cljs?}}]
-  :plugins [[duct/lein-plugin "0.9.0-SNAPSHOT"]]
+  :plugins [[duct/lein-duct "0.9.0-SNAPSHOT"]]
   :main ^:skip-aot {{namespace}}.main{{#uberjar-name}}
   :uberjar-name   "{{uberjar-name}}"{{/uberjar-name}}
   :resource-paths ["resources" "target/resources"]
