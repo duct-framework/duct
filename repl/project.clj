854211d3d6e29285a812476e59276e6500228d2e
James Reeves <jreeves@weavejester.com>
2017-03-09 02:01:39 +0000

    Update Integrant dependency to 0.2.3

--- repl/project.clj
+++ repl/project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [eftest "0.1.2"]
                  [duct/core "0.9.0-SNAPSHOT"]
-                 [integrant/repl "0.1.0"]])
+                 [integrant/repl "0.1.1"]])
