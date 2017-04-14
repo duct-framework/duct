ac46a0c9ae078cee99c159f125da97d787effe08
James Reeves <jreeves@weavejester.com>
2017-04-14 22:54:24 +0100

    Remove duct/core as it has its own repo

--- core/src/duct/core/protocols.clj
+++
@@ -1,9 +0,0 @@
-(ns duct.core.protocols)
-
-(defprotocol Logger
-  "Protocol for abstracting logging. Used by the duct.core/log macro."
-  (-log [logger level ns-str file line event data]))
-
-(extend-protocol Logger
-  nil
-  (-log [_ _ _ _ _ _ _]))
--- core/src/duct/core/repl.clj
+++
@@ -1,7 +0,0 @@
-(ns duct.core.repl
-  (:refer-clojure :exclude [test])
-  (:require [eftest.runner :as eftest]))
-
-(defn test []
-  (eftest/run-tests (eftest/find-tests "test") {:multithread? false})
-  nil)
