31aaae8d43f9dfe149a1989eb7bf052d2f63d231
James Reeves <jreeves@weavejester.com>
2017-04-18 02:16:38 +0100

    Update project info in project files

--- lein-duct/project.clj
+++ lein-duct/project.clj
@@ -1,6 +1,7 @@
 (defproject duct/lein-duct "0.9.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A plugin to be used in the Duct Leiningen template"
+  :url "https://github.com/duct-framework/duct"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,6 +1,6 @@
 (defproject duct/lein-template "0.9.0-SNAPSHOT"
-  :description "A template for a component-based web application."
-  :url "https://github.com/weavejester/duct"
+  :description "A template for an Integrant-based web application"
+  :url "https://github.com/duct-framework/duct"
   :scm {:dir ".."}
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
