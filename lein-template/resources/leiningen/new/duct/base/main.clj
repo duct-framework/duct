dd44aea35c6c3f794ea33193c1ef0d361f09d1df
James Reeves <jreeves@weavejester.com>
2017-05-09 02:51:56 +0100

    Fix indentation of main.clj template

--- lein-template/resources/leiningen/new/duct/base/main.clj
+++ lein-template/resources/leiningen/new/duct/base/main.clj
@@ -1,7 +1,7 @@
 (ns {{namespace}}.main
-    (:gen-class)
-    (:require [clojure.java.io :as io]
-              [duct.core :as duct]))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [duct.core :as duct]))
 
 (defn -main [& args]
   (duct/exec (duct/read-config (io/resource "{{dirs}}/config.edn"))))
