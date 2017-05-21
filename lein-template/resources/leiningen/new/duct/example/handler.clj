fbc17a274cf5540c19ff4ade0f99165e56e664a2
James Reeves <jreeves@weavejester.com>
2017-05-21 14:28:38 +0100

    Neaten up example handler template

--- lein-template/resources/leiningen/new/duct/example/handler.clj
+++ lein-template/resources/leiningen/new/duct/example/handler.clj
@@ -4,10 +4,7 @@
             [clojure.java.io :as io]<</site?>>
             [integrant.core :as ig]))
 
-(defn example-handler [options]
+(defmethod ig/init-key :<<namespace>>.handler/example [_ options]
   (context "/example" []
     (GET "/" []
       <<^site?>>"This is an example handler"<</site?>><<#site?>>(io/resource "<<dirs>>/handler/example/example.html")<</site?>>)))
-
-(defmethod ig/init-key :<<namespace>>.handler/example [_ options]
-  (example-handler options))
--- lein-template/resources/leiningen/new/duct/example/handler_test.clj
+++ lein-template/resources/leiningen/new/duct/example/handler_test.clj
@@ -2,10 +2,11 @@
   (:require [clojure.test :refer :all]
             [kerodon.core :refer :all]
             [kerodon.test :refer :all]
+            [integrant.core :as ig]
             [{{namespace}}.handler.example :as example]))
 
 (def handler
-  (example/example-handler {}))
+  (ig/init-key :{{namespace}}.handler/example {}))
 
 (deftest smoke-test
   (testing "example page exists"
