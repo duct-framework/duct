152bf28942fa36141cc98a58574e8ee1aedf4ad0
James Reeves <jreeves@weavejester.com>
2017-11-02 18:35:22 +0000

    Fix example handler test in template

--- lein-template/resources/leiningen/new/duct/example/handler_test.clj
+++ lein-template/resources/leiningen/new/duct/example/handler_test.clj
@@ -1,15 +1,11 @@
 (ns {{namespace}}.handler.example-test
   (:require [clojure.test :refer :all]
-            [kerodon.core :refer :all]
-            [kerodon.test :refer :all]
             [integrant.core :as ig]
+            [ring.mock.request :as mock]
             [{{namespace}}.handler.example :as example]))
-
-(def handler
-  (ig/init-key :{{namespace}}.handler/example {}))
 
 (deftest smoke-test
   (testing "example page exists"
-    (-> (session handler)
-        (visit "/example")
-        (has (status? 200) "page exists"))))
+    (let [handler  (ig/init-key :{{namespace}}.handler/example {})
+          response (handler (mock/request :get "/example"))]
+      (is {{#ataraxy?}}(= :ataraxy.response/ok (first response)){{/ataraxy?}}{{^ataraxy?}}(= 200 (:status response)){{/ataraxy?}} "response ok"))))
