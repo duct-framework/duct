7a82f57f086c8d0248e5581f25bf522c22ef4e96
James Reeves <jreeves@weavejester.com>
2018-10-01 19:49:44 +0100

    Remove unnecessary requires from main.clj

--- lein-template/resources/leiningen/new/duct/base/main.clj
+++ lein-template/resources/leiningen/new/duct/base/main.clj
@@ -1,14 +1,12 @@
 (ns {{namespace}}.main
   (:gen-class)
-  (:require [clojure.java.io :as io]
-            [duct.core :as duct]
-            [integrant.core :as ig]))
+  (:require [duct.core :as duct]))
 
 (duct/load-hierarchy)
 
 (defn -main [& args]
   (let [keys     (or (duct/parse-keys args) [:duct/daemon])
         profiles [:duct.profile/prod]]
-    (-> (io/resource "{{dirs}}/config.edn")
+    (-> (duct/resource "{{dirs}}/config.edn")
         (duct/read-config)
         (duct/exec-config profiles keys))))
