8b3ecb17f72050df2f9795bf6493424faec13570
James Reeves <jreeves@weavejester.com>
2018-10-31 18:19:03 +0000

    Update template dependencies

--- lein-template/resources/leiningen/new/duct/base/project.clj
+++ lein-template/resources/leiningen/new/duct/base/project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.10.0-beta4"]
                  [duct/core "0.7.0-beta1"]
-                 [duct/module.logging "0.4.0-alpha1"]{{#web?}}
-                 [duct/module.web "0.7.0-alpha4"]{{/web?}}{{#ataraxy?}}
+                 [duct/module.logging "0.4.0-beta1"]{{#web?}}
+                 [duct/module.web "0.7.0-beta1"]{{/web?}}{{#ataraxy?}}
                  [duct/module.ataraxy "0.3.0-alpha3"]{{/ataraxy?}}{{#cljs?}}
                  [duct/module.cljs "0.4.0-beta1"]{{/cljs?}}{{#jdbc?}}
                  [duct/module.sql "0.5.0-alpha1"]{{/jdbc?}}{{#postgres?}}
                  [org.postgresql/postgresql "42.2.5"]{{/postgres?}}{{#sqlite?}}
-                 [org.xerial/sqlite-jdbc "3.23.1"]{{/sqlite?}}]
+                 [org.xerial/sqlite-jdbc "3.25.2"]{{/sqlite?}}]
   :plugins [[duct/lein-duct "0.11.0-beta1"]]
   :main ^:skip-aot {{namespace}}.main{{#uberjar-name}}
   :uberjar-name  "{{uberjar-name}}"{{/uberjar-name}}
