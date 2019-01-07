48b11aa48e5a659e9c9b1a973039aca886ef981e
James Reeves <jreeves@weavejester.com>
2019-01-07 13:55:39 +0000

    Update template dependencies

--- lein-template/resources/leiningen/new/duct/base/project.clj
+++ lein-template/resources/leiningen/new/duct/base/project.clj
@@ -2,13 +2,13 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0-beta4"]
-                 [duct/core "0.7.0-beta2"]
-                 [duct/module.logging "0.4.0-beta1"]{{#web?}}
-                 [duct/module.web "0.7.0-beta1"]{{/web?}}{{#ataraxy?}}
-                 [duct/module.ataraxy "0.3.0-alpha3"]{{/ataraxy?}}{{#cljs?}}
-                 [duct/module.cljs "0.4.0-beta1"]{{/cljs?}}{{#jdbc?}}
-                 [duct/module.sql "0.5.0-beta1"]{{/jdbc?}}{{#postgres?}}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [duct/core "0.7.0"]
+                 [duct/module.logging "0.4.0"]{{#web?}}
+                 [duct/module.web "0.7.0"]{{/web?}}{{#ataraxy?}}
+                 [duct/module.ataraxy "0.3.0"]{{/ataraxy?}}{{#cljs?}}
+                 [duct/module.cljs "0.4.0"]{{/cljs?}}{{#jdbc?}}
+                 [duct/module.sql "0.5.0"]{{/jdbc?}}{{#postgres?}}
                  [org.postgresql/postgresql "42.2.5"]{{/postgres?}}{{#sqlite?}}
                  [org.xerial/sqlite-jdbc "3.25.2"]{{/sqlite?}}]
   :plugins [[duct/lein-duct "0.11.0-beta4"]]
@@ -26,5 +26,5 @@
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
                   :dependencies   [[integrant/repl "0.3.1"]
-                                   [eftest "0.5.3"]{{#web?}}
+                                   [eftest "0.5.4"]{{#web?}}
                                    [kerodon "0.9.0"]{{/web?}}]}})
